197aeac7d41fb49d6ded8b34faa95cae03d2c895
Vagiz Musin <vagiz_mus@mail.ru>
2020-10-13 21:02:57 +0600

    feat: add code task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let left = [];
+  let right = [];
+  let arr = [];
+  let brackets = bracketsConfig.toString().split(',');
+  for (let i=0; i<brackets.length; i++) {
+      if (i % 2 == 0) {
+          right.push(brackets[i]);
+      } else if (i % 2 != 0) {
+          left.push(brackets[i]);
+      } 
+  }
+  for (j=0; j<str.length; j++) {
+      if (right.includes(str[j])) {
+          arr.push(str[j]);
+      } else if (left.indexOf(str[j]) === right.indexOf(arr[arr.length-1])) {
+          arr.pop();
+      } else {
+         return false;
+      }
+  }
+ return arr.length === 0;
 }
